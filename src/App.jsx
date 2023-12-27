6ecb4898d3a94d7eb2f1dafbc501bea34c5dbcb3
KRISHNA VAMSI R <krishnavamsi.190120@gmail.com>
2023-12-27 17:18:21 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Krishna Vamsi Regulavalasa",
+  title: "Alpha Ambassador, MLSA",
+  email: "Krishnavamsi.Regulavalasa@studentambassadors.com",
+  gitHub: "999krish",
+  linkedIn: "krishna190320",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Laptop.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desk with Laptop and Notebook";
 
 /**
  * Project list
@@ -30,23 +30,18 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */
 const projectList = [
+  
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My GitHub Profile",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my Githyb porfolio , where I have Contributed Many Opensource projects.",
+    url: "https://github.com/999krish",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Hers's my resume.",
+    url: "https://drive.google.com/file/d/1gjwTfCO9rhsp7HgBE9najTvhhK8mjRcP/view?usp=sharing",
   },
   {
     title: "GitHub Codespaces and github.dev",
